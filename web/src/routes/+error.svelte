80e57ed7e35abfb678c40201fa846d60988b0fbc
Andrew Thomas <andrewmthomas87@gmail.com>
2023-12-03 01:34:52 -0600

    Add error page

---
+++ web/src/routes/+error.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import { page } from '$app/stores';
+</script>
+
+<main>
+  <section class="section">
+    <div class="container">
+      <h1 class="title">Status {$page.status}</h1>
+      <h2 class="subtitle">{$page.error?.message}</h2>
+
+      <a href="/" class="button is-link is-light">Return to home</a>
+    </div>
+  </section>
+</main>
